994a61f5b00e55f00ee2ff53c9d5bff8fa5206b8
Samueldenekew390 <samueldenekew19@gmail.com>
2026-09-23 10:59:06 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+import './config.js'
+import './app.js'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
